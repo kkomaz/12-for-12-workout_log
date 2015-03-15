8393d4f00ea87c50ebc058a98da0b3d9f74a26f1
kkomaz <leeajl006@gmail.com>
2015-03-15 14:27:14 -0400

    change date time formatting

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,6 +1,6 @@
 class ExercisesController < ApplicationController
   def create
-    binding.pry
+    # binding.pry
     @workout = Workout.find(params[:workout_id])
     @exercise = @workout.exercises.create(params[:exercise].permit(:name, :sets, :reps))
     redirect_to workout_path(@workout)
